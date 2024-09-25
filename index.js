8d1eb5a17dc09f281f2803fd5e8a676bc128ff31
JasperMunene <jaspermunene2022@gmail.com>
2024-09-25 19:05:42 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const scuberDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = (array) => {
+    let newArray = array.slice(0,2)
+    return newArray
+}
+
+const returnLastTwoDrivers = (array) => {
+    let newArray = array.slice(-2)
+    return newArray
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (multiplier) => {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+  
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers, fn) => {
+    return fn(arrayOfDrivers);
+  };
+  
